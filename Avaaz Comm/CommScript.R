692a8a25a71a746c2b8d02cde06a27dcbaaeaad4
fcalv <fracalvetti1@gmail.com>
2020-03-12 15:38:11 +0100

    imac commit 2

--- Avaaz Comm/CommScript.R
+++ Avaaz Comm/CommScript.R
@@ -3,4 +3,15 @@ library(tidyverse)
 
 setwd(dirname(getSourceEditorContext()$path))
 
+###########
+# READ FILES
 
+fau <- "videoinfo_EhW-B2udhQw_2020_03_12-12_31_32_authors.tab"
+fbi <- "videoinfo_EhW-B2udhQw_2020_03_12-12_31_32_basicinfo.tab"
+fcm <- "videoinfo_EhW-B2udhQw_2020_03_12-12_31_32_comments.tab"
+
+dfcomm <- fcm %>% read.table(sep = '\t', header = T, fill = T)
+dfbas <- fbi %>% read.table(sep = '\t', header = T)
+dfauth <- fau %>% read.table(sep = '\t', header = T)
+
+############
